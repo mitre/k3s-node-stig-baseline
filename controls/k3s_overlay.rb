91253ae64015174d4fc5989ccb9f8a957baaa78b
Rony Xavier <rx294@nyu.edu>
2021-05-06 17:02:41 -0400

    inital code push
    Signed-off-by: Rony Xavier <rx294@nyu.edu>

---
+++ controls/k3s_overlay.rb
@@ -0,0 +1,28 @@
+include_controls 'k8s-node-stig-baseline' do
+  control 'CNTR-K8-000860' do
+    impact 0.0
+    desc 'caveat', 'K3S deployments are not applied using manifest files and hence non-applicable.'
+  end
+  control 'CNTR-K8-000900' do
+    impact 0.0
+    desc 'caveat', 'K3S deployments are not applied using manifest files and hence non-applicable.'
+  end
+  control 'CNTR-K8-003110' do
+    impact 0.0
+    desc 'caveat', 'K3S deployments are not applied using manifest files and hence non-applicable.'
+  end
+  control 'CNTR-K8-003250' do
+    impact 0.0
+    desc 'caveat', 'K3S deployments are not applied using manifest files and hence non-applicable.'
+  end
+  control 'CNTR-K8-003210' do
+    impact 0.0
+    desc 'caveat', 'K3S deployments are not configured through Kubeadm and hence non-applicable.'
+  end
+  control 'CNTR-K8-003220' do
+    impact 0.0
+    desc 'caveat', 'K3S deployments are not configured through Kubeadm and hence non-applicable.'
+  end
+
+
+end
