55921e9cb862e16efd37b1122112a7150bef4197
Rony Xavier <rx294@nyu.edu>
2021-05-20 06:53:31 -0400

    Updates 1) Convert control IDs from STIG IDs to Vuln IDs 2) Update V-242429 V-242430 V-242431 to use datastore based reference specific to k3s
    Signed-off-by: Rony Xavier <rx294@nyu.edu>

--- controls/k3s_overlay.rb
+++ controls/k3s_overlay.rb
@@ -1,26 +1,57 @@
 include_controls 'k8s-node-stig-baseline' do
-  control 'CNTR-K8-000860' do
+  control 'V-242405' do
     impact 0.0
     desc 'caveat', 'K3S deployments are not applied using manifest files and hence non-applicable.'
   end
-  control 'CNTR-K8-000900' do
+  control 'V-242408' do
     impact 0.0
     desc 'caveat', 'K3S deployments are not applied using manifest files and hence non-applicable.'
   end
-  control 'CNTR-K8-003110' do
+  control 'V-242444' do
     impact 0.0
     desc 'caveat', 'K3S deployments are not applied using manifest files and hence non-applicable.'
   end
-  control 'CNTR-K8-003250' do
+  control 'V-242458' do
     impact 0.0
     desc 'caveat', 'K3S deployments are not applied using manifest files and hence non-applicable.'
   end
-  control 'CNTR-K8-003210' do
+  control 'V-242454' do
     impact 0.0
     desc 'caveat', 'K3S deployments are not configured through Kubeadm and hence non-applicable.'
   end
-  control 'CNTR-K8-003220' do
+  control 'V-242455' do
     impact 0.0
     desc 'caveat', 'K3S deployments are not configured through Kubeadm and hence non-applicable.'
+  end
+
+  control 'V-242429' do
+    unless kube_apiserver.exist?
+      impact 0.0
+      desc 'caveat', 'Kubernetes API Server process is not running on the target.'
+    end
+
+    describe kube_apiserver do
+      its('datastore-cafile') { should_not be_nil }
+    end
+  end
+  control 'V-242430' do
+    unless kube_apiserver.exist?
+      impact 0.0
+      desc 'caveat', 'Kubernetes API Server process is not running on the target.'
+    end
+
+    describe kube_apiserver do
+      its('datastore-certfile') { should_not be_nil }
+    end
+  end
+  control 'V-242431' do
+    unless kube_apiserver.exist?
+      impact 0.0
+      desc 'caveat', 'Kubernetes API Server process is not running on the target.'
+    end
+
+    describe kube_apiserver do
+      its('datastore-keyfile') { should_not be_nil }
+    end
   end
 end
